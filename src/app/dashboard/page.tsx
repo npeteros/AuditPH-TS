056bb21b212ade56d0cfc88a5d6ffabc734fa03e
Seaside <n.peteros2003@gmail.com>
2023-11-27 22:55:25 +0800

    Redirection dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,6 +7,7 @@ import { redirect } from "next/navigation";
 
 export default async function Page() {
     const session = await auth();
+    if(!session) return redirect('/login');
     const user = await prisma.user.findUnique({
         where: {
             email: session?.user?.email ?? undefined
