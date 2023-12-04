527ae855d531b896bd067cf451b3d347d78cd545
Seaside <n.peteros2003@gmail.com>
2023-12-04 17:58:04 +0800

    Revised profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,29 +1,20 @@
-
-
-
-
-
-
-
-
-
 export default function Page() {
     return (
         <>
-            <div className="bg-white max-w-6xl mx-auto py-2 m-14 rounded-xl">
-                <div className="m-5 pb-10">
-                    <p className="text-xl text-gray-900">Profile Information</p>
-                    <p className="text-medium text-gray-600">Update your account's profile information and email address.</p>
+            <div className="bg-white max-w-6xl mx-auto py-2 mt-14 rounded-xl">
+                <div className="m-5">
+                    <p className="text-lg text-gray-900 font-semibold">Profile Information</p>
+                    <p className="text-sm text-gray-600">Update your account&apos;s profile information and email address.</p>
                 </div>
                 <div> {/* for name and email div */}
                     <div className="m-5">
-                        <p className="text-gray-900 text-xl pb-1">
+                        <p className="text-gray-900 text-md pb-1">
                             Name
                         </p>
                         <input className="border border-gray-300 p-3 w-full rounded-md" type="text" placeholder="type here..." />
                     </div>
-                    <div className="m-5 pt-6">
-                        <p className="text-gray-900 text-xl pb-1">
+                    <div className="m-5">
+                        <p className="text-gray-900 text-md pb-1">
                             Email
                         </p>
                         <input className="border border-gray-300 p-3 w-full rounded-md" type="text" placeholder="ex: asd@gmail.com" />
@@ -35,12 +26,12 @@ export default function Page() {
             </div>
 
 
-            <div className="bg-white max-w-6xl mx-auto py-2 m-14 rounded-xl">
+            {/* <div className="bg-white max-w-6xl mx-auto py-2 m-14 rounded-xl">
                 <div className="m-5 pb-10">
                     <p className="text-xl text-gray-900">Update Password</p>
                     <p className="text-medium text-gray-600">Ensure your account is using a long random password to stay secure.</p>
                 </div>
-                <div> {/* for name and email div */}
+                <div>
                     <div className="m-5">
                         <p className="text-gray-900 text-xl pb-1">
                             Current Password
@@ -63,7 +54,7 @@ export default function Page() {
                 <button className="bg-gray-900 m-5 rounded-md px-4 py-2 text-white ">
                     SAVE
                 </button>
-            </div>
+            </div> */}
 
 
 
