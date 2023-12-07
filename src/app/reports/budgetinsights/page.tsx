fd283578904bb1c2cabbb8ee16fce79ab3c865e7
BrettKho <21104078@usc.edu.ph>
2023-12-07 18:04:49 +0800

    made and styled budgetinsights

--- src/app/reports/budgetinsights/page.tsx
+++ src/app/reports/budgetinsights/page.tsx
@@ -1,4 +1,9 @@
-export default function Page() {
+"use client";
+
+import React from 'react';
+import ChartComponent from './ChartComponent';
+
+const App = () => {
     const data = [
         { budget: "Bills", total: 20000 },
         { budget: "Personal", total: 34122 },
@@ -8,9 +13,18 @@ export default function Page() {
         { budget: "Pets", total: 41242 },
         { budget: "Groceries", total: 21242 },
     ];
+
+
     return (
-        <div>
-            Hello world!
+        <div className='w-8/12 mx-auto'>
+            <div className='w-3/5 mx-auto'>
+                <p className='text-6xl text-center text-lime-100 font-semibold p-4'>TOTAL EXPENDITURES</p>
+            </div>
+            <div className='bg-white rounded-lg p-4'>
+                <ChartComponent data={data} />
+            </div>
         </div>
-    )
-}
+    );
+};
+
+export default App;
